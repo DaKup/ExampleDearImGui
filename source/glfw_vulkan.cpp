db87dc4bb75f798071530b448e48c289c32f458a
Daniel <daniel.kup@student.tugraz.at>
2022-08-17 17:14:40 +0200

    unused variables

--- source/glfw_vulkan.cpp
+++ source/glfw_vulkan.cpp
@@ -168,8 +168,8 @@ Application::~Application()
 void Application::RunMainLoop()
 {
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
+    //bool show_demo_window = true;
+    //bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
     // Main loop
     while (!glfwWindowShouldClose(m_glfw_window))
